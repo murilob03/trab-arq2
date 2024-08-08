c314886339acb31b8893b254cb1a411004ac4e98
murilob03 <muriloviskboccardo@gmail.com>
2024-08-08 15:56:08 -0300

    melhora legibilidade em alguns arquivos

--- blood_bank/BloodBank.py
+++ blood_bank/BloodBank.py
@@ -3,52 +3,51 @@ from enums import BloodType
 
 
 class BloodBank:
+    """Handles blood bank operations using the MESI protocol."""
+
     def __init__(self, simulator: MESISimulator):
         self.mesi_simulator = simulator
 
-    def use_blood(self, hospital_id: int, blood_id: int, blood_type_needed):
-        if blood_type_needed == "E":
+    def use_blood(self, hospital_id: int, blood_id: int, required_blood_type: str):
+        """Uses blood from a specified bag if it matches the needed type."""
+        if required_blood_type == "E":
             return "You can't use blood from an empty bag!"
+
         data = self.mesi_simulator.caches[hospital_id].read(blood_id)
+        available_blood = data.data[blood_id % 5]  # type: ignore
 
-        blood_available = data.data[blood_id % 5]  # type: ignore
-
-        if blood_available.value != blood_type_needed:  # type: ignore
+        if available_blood.value != required_blood_type:  # type: ignore
             return "Blood requested is not available anymore."
 
         self.mesi_simulator.caches[hospital_id].write(blood_id, BloodType("E"))
-
         return "Transaction successful."
 
-    def request_blood(self, unit_id: int, blood_id: int):
-        data = self.mesi_simulator.caches[unit_id].read(blood_id)
-
+    def request_blood(self, hospital_id: int, blood_id: int):
+        """Requests the type of blood in a specified bag."""
+        data = self.mesi_simulator.caches[hospital_id].read(blood_id)
         blood_type = data.data[blood_id % 5].value  # type: ignore
+
         if blood_type != "E":  # type: ignore
             return f"The type of the blood in bag {blood_id} is {blood_type}."
-        else:
-            return f"The bag number {blood_id} is empty."
+        return f"The bag number {blood_id} is empty."
 
-    def donate_blood(self, hospital_id: int, blood_donated):
-        # Try to find an empty bag
-        addr = 0
-        block = self.mesi_simulator.caches[hospital_id].read(addr)
-        found = False
-        while addr < self.mesi_simulator.main_memory.n_lines - 4:
-            if BloodType("E") in block.data:
-                found = True
-                break
-            addr += 5
-            block = self.mesi_simulator.caches[hospital_id].read(addr)
-
-        if not found:
+    def donate_blood(self, hospital_id: int, donated_blood_type: str):
+        """Donates blood to an empty bag in the bank."""
+        empty_bag_address = self._find_empty_bag(hospital_id)
+        if empty_bag_address is None:
             return "The bank is out of empty bags!"
 
-        for i in range(len(block.data)):
-            if block.data[i].value == "E":
-                addr += i
-                break
+        self.mesi_simulator.caches[hospital_id].write(
+            empty_bag_address, BloodType(donated_blood_type)
+        )
+        return f"Blood accepted at bag number {empty_bag_address}."
 
-        self.mesi_simulator.caches[hospital_id].write(addr, BloodType(blood_donated))
-
-        return f"Blood accepted at bag number {addr}."
+    def _find_empty_bag(self, hospital_id: int):
+        """Finds an empty bag address in the blood bank."""
+        for addr in range(0, self.mesi_simulator.main_memory.n_lines, 5):
+            block = self.mesi_simulator.caches[hospital_id].read(addr)
+            if BloodType("E") in block.data:  # type: ignore
+                for i, blood in enumerate(block.data):  # type: ignore
+                    if blood.value == "E":  # type: ignore
+                        return addr + i
+        return None
--- blood_bank/BloodBankGUI.py
+++ blood_bank/BloodBankGUI.py
@@ -1,66 +1,60 @@
 import tkinter as tk
 from tkinter import ttk
 import tkinter.font as tkFont
-import sys
-
 from enums import BloodType
 from mesi_simulator import MESISimulator
 from blood_bank.BloodBank import BloodBank
 
 
 class OutputBox:
+    """Handles output logging to a text widget."""
+
     def __init__(self):
         self.widget = None
 
     def write(self, message):
-        self.widget.configure(state="normal")
-        self.widget.insert(tk.END, message)
-        self.widget.configure(state="disabled")
-        self.widget.see(tk.END)
+        """Writes a message to the attached text widget."""
+        if self.widget:
+            self.widget.configure(state="normal")
+            self.widget.insert(tk.END, message)
+            self.widget.configure(state="disabled")
+            self.widget.see(tk.END)
+        else:
+            raise Exception("A text box must be attached before using this function!")
 
     def attach_text_box(self, text_box):
+        """Attaches a text widget to the OutputBox for logging."""
         self.widget = text_box
 
 
 class BloodBankGUI:
+    """GUI class for the Blood Bank simulator using the MESI protocol."""
+
     def __init__(self, simulator: MESISimulator):
         self.blood_bank = BloodBank(simulator)
-
         self.main_memory = simulator.main_memory
         self.caches = simulator.caches
         self.MAIN_MEMORY_SIZE = simulator.main_memory.n_lines
-        self.BLOCK_SIZE = simulator.main_memory.block_size
-
         self.tables = []
-        self.processor_map = {}
-        for i in range(len(self.caches)):
-            self.processor_map[f"Hospital {i + 1}"] = i
-
+        self.processor_map = {f"Hospital {i + 1}": i for i in range(len(self.caches))}
         self.output = OutputBox()
-
         self.root = tk.Tk()
         self.root.title("MESI Simulator")
-
         self.setup_ui()
 
     def refresh_tables(self):
+        """Refreshes the data displayed in the tables."""
         for table in self.tables:
-            for item in table.get_children():
-                table.delete(item)
+            table.delete(*table.get_children())
 
+        # Populate main memory table
         for i in range(self.MAIN_MEMORY_SIZE):
-            if i % 10 < 5:
-                self.tables[0].insert(
-                    "", "end", values=(i, self.main_memory.data[i]), tags=("fixed")
-                )
-            else:
-                self.tables[0].insert(
-                    "",
-                    "end",
-                    values=(i, self.main_memory.data[i]),
-                    tags=("lightgray", "fixed"),
-                )
+            tag = "lightgray" if i % 10 >= 5 else "fixed"
+            self.tables[0].insert(
+                "", "end", values=(i, self.main_memory.data[i]), tags=(tag,)
+            )
 
+        # Populate cache table for the selected hospital
         active_hospital = self.processor_map[self.hospital_combobox.get()]
         for addr in reversed(self.caches[active_hospital].queue):
             data = " | ".join(
@@ -69,17 +63,13 @@ class BloodBankGUI:
             self.tables[1].insert(
                 "",
                 "end",
-                values=(
-                    addr,
-                    data,
-                    self.caches[active_hospital].data[addr].tag.value,
-                ),
+                values=(addr, data, self.caches[active_hospital].data[addr].tag.value),
                 tags=("fixed",),
             )
 
     def use_blood(self):
+        """Handles the use blood action."""
         hospital = self.processor_map[self.hospital_combobox.get()]
-
         selected_item = self.tables[0].selection()[0]
         values = self.tables[0].item(selected_item, "values")
         blood_id = int(values[0])
@@ -93,6 +83,7 @@ class BloodBankGUI:
         self.refresh_tables()
 
     def donate_blood(self):
+        """Handles the donate blood action."""
         hospital = self.processor_map[self.hospital_combobox.get()]
         blood_type = self.blood_combobox.get().strip()
 
@@ -101,12 +92,11 @@ class BloodBankGUI:
         )
         self.output.write(self.blood_bank.donate_blood(hospital, blood_type))
         self.output.write("\n\n")
-
         self.refresh_tables()
 
     def request_blood(self):
+        """Handles the request blood action."""
         hospital = self.processor_map[self.hospital_combobox.get()]
-
         selected_item = self.tables[0].selection()[0]
         values = self.tables[0].item(selected_item, "values")
         blood_id = int(values[0])
@@ -119,13 +109,13 @@ class BloodBankGUI:
         self.refresh_tables()
 
     def setup_ui(self):
+        """Sets up the UI components."""
         fixed_font = tkFont.Font(family="Courier New", size=10)
-
         control_frame = tk.Frame(self.root)
         control_frame.pack(pady=10)
 
-        blood_label = tk.Label(control_frame, text="Value:")
-        blood_label.pack(side=tk.LEFT, padx=5, pady=5)
+        # Blood type combobox
+        tk.Label(control_frame, text="Value:").pack(side=tk.LEFT, padx=5, pady=5)
         blood_options = [str(x) for x in list(BloodType)]
         self.blood_combobox = ttk.Combobox(
             control_frame, values=blood_options, width=4, state="readonly"
@@ -133,8 +123,8 @@ class BloodBankGUI:
         self.blood_combobox.pack(side=tk.LEFT, padx=5, pady=5)
         self.blood_combobox.current(0)
 
-        hospital_label = tk.Label(control_frame, text="Processor:")
-        hospital_label.pack(side=tk.LEFT, padx=5, pady=5)
+        # Hospital combobox
+        tk.Label(control_frame, text="Processor:").pack(side=tk.LEFT, padx=5, pady=5)
         hospital_options = list(self.processor_map.keys())
         self.hospital_combobox = ttk.Combobox(
             control_frame, values=hospital_options, width=12, state="readonly"
@@ -142,50 +132,49 @@ class BloodBankGUI:
         self.hospital_combobox.pack(side=tk.LEFT, padx=5, pady=5)
         self.hospital_combobox.current(0)
 
-        request_blood = tk.Button(
-            control_frame, text="Request Blood", command=self.request_blood
+        # Action buttons
+        tk.Button(control_frame, text="Request Blood", command=self.request_blood).pack(
+            side=tk.LEFT, padx=5, pady=5
         )
-        request_blood.pack(side=tk.LEFT, padx=5, pady=5)
-
-        use_blood_button = tk.Button(
-            control_frame, text="Use Blood", command=self.use_blood
+        tk.Button(control_frame, text="Use Blood", command=self.use_blood).pack(
+            side=tk.LEFT, padx=5, pady=5
         )
-        use_blood_button.pack(side=tk.LEFT, padx=5, pady=5)
-
-        donate_button = tk.Button(
-            control_frame, text="Donate Blood", command=self.donate_blood
+        tk.Button(control_frame, text="Donate Blood", command=self.donate_blood).pack(
+            side=tk.LEFT, padx=5, pady=5
         )
-        donate_button.pack(side=tk.LEFT, padx=5, pady=5)
 
+        # Table frame for data display
         table_frame = tk.Frame(self.root)
         table_frame.pack(pady=10)
 
-        table_label = tk.Label(table_frame, text="Blood Bank")
-        table_label.grid(row=0, column=0, sticky="n", padx=(15, 0))
-
-        table = ttk.Treeview(
+        # Main memory table
+        tk.Label(table_frame, text="Blood Bank").grid(
+            row=0, column=0, sticky="n", padx=(15, 0)
+        )
+        memory_table = ttk.Treeview(
             table_frame, columns=("Col1", "Col2"), show="headings", height=40
         )
-        table.heading("Col1", text="Address")
-        table.heading("Col2", text="Value")
-        table.column("Col1", width=80, anchor="center")
-        table.column("Col2", width=80, anchor="center")
-        table.grid(
+        memory_table.heading("Col1", text="Address")
+        memory_table.heading("Col2", text="Value")
+        memory_table.column("Col1", width=80, anchor="center")
+        memory_table.column("Col2", width=80, anchor="center")
+        memory_table.grid(
             row=0, column=0, rowspan=2, sticky="nswe", padx=(10, 0), pady=(25, 5)
         )
-        table.tag_configure("lightgray", background="#d3d3d3")
-        table.tag_configure("fixed", font=fixed_font)
+        memory_table.tag_configure("lightgray", background="#d3d3d3")
+        memory_table.tag_configure("fixed", font=fixed_font)
+        self.tables.append(memory_table)
 
-        scrollbar = tk.Scrollbar(table_frame, orient="vertical", command=table.yview)
-        scrollbar.grid(
+        memory_scrollbar = tk.Scrollbar(
+            table_frame, orient="vertical", command=memory_table.yview
+        )
+        memory_scrollbar.grid(
             row=0, column=1, rowspan=2, padx=(0, 5), pady=(25, 5), sticky="ns"
         )
+        memory_table.configure(yscrollcommand=memory_scrollbar.set)
 
-        table.configure(yscrollcommand=scrollbar.set)
-        self.tables.append(table)
-
+        # Cache table for the selected hospital
         active_hospital = self.processor_map[self.hospital_combobox.get()]
-
         cache_table_label = tk.Label(
             table_frame, text=f"Hospital {active_hospital + 1} Cache"
         )
@@ -211,11 +200,11 @@ class BloodBankGUI:
             self.refresh_tables()
 
         self.hospital_combobox.bind("<<ComboboxSelected>>", on_combobox_change)
-        self.refresh_tables()
 
-        console_label = tk.Label(table_frame, text="Output")
-        console_label.grid(row=0, column=len(self.caches), sticky="n")
-
+        # Output console
+        tk.Label(table_frame, text="Output").grid(
+            row=0, column=len(self.caches), sticky="n"
+        )
         console = tk.Text(table_frame, wrap="word", width=50)
         console.grid(
             row=0,
@@ -225,9 +214,10 @@ class BloodBankGUI:
             padx=(5, 0),
             pady=(25, 5),
         )
-
-        scrollbar = tk.Scrollbar(table_frame, orient="vertical", command=console.yview)
-        scrollbar.grid(
+        console_scrollbar = tk.Scrollbar(
+            table_frame, orient="vertical", command=console.yview
+        )
+        console_scrollbar.grid(
             row=0,
             column=len(self.caches) + 1,
             rowspan=2,
@@ -235,8 +225,11 @@ class BloodBankGUI:
             pady=(25, 5),
             sticky="ns",
         )
-
+        console.configure(yscrollcommand=console_scrollbar.set)
         self.output.attach_text_box(console)
 
+        self.refresh_tables()
+
     def mainloop(self):
+        """Starts the main loop of the Tkinter application."""
         self.root.mainloop()
